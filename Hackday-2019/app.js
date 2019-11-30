b94b8107b3c005ed6be4e32d2116b1542219a80e
Meri <twinkling.star.m.t@gmail.com>
2019-11-30 13:56:24 +0900

    feat: login with twitter(continue)

--- Hackday-2019/app.js
+++ Hackday-2019/app.js
@@ -1,11 +1,18 @@
+//require("@babel/register");
+//require("babel-polyfill");
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var TwitterStrategy = require('passport-twitter').Strategy;
+var passport = require('passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth.js');
 
 var app = express();
 
@@ -18,9 +25,41 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+app.get('/auth/twitter', passport.authenticate('twitter'));
+app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/?auth_failed' }),
+  function (req, res) {
+    res.redirect('/auth/success');
+});
+
+// passport-twitterの設定
+passport.use(new TwitterStrategy({
+  consumerKey: process.env.TWITTER_CONSUMER_KEY,
+  consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+  callbackURL: process.env.TWITTER_CALLBACK_URL
+},
+  // 認証後の処理
+  function (token, tokenSecret, profile, done) {
+    return done(null, profile);
+  }
+));
+// セッションに保存
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Hackday-2019/routes/auth.js
+++ Hackday-2019/routes/auth.js
@@ -1,9 +1,53 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+//const CryptoJS = require("crypto-js");
+const oauth = require('oauth');
+const _twitterConsumerKey = process.env.TWITTER_CONSUMER_KEY;
+const _twitterConsumerSecret = process.env.TWITTER_CONSUMER_SECRET;
+const _twitterCallbackUrl = process.env.TWITTER_CALLBACK_URL;
+const _twitterAccessToken = process.env.TWITTER_ACCESS_TOCKEN;
+const _twitteraccessTokenSecret = process.env.TWITTER_ACCESS_SECRET;
 
-/* GET login listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+const consumer = new oauth.OAuth("https://twitter.com/oauth/request_token", "https://twitter.com/oauth/access_token", _twitterConsumerKey, _twitterConsumerSecret, "1.0A", _twitterCallbackUrl, "HMAC-SHA1");
+
+router.get('/login/success', (req, res) => {
+  res.send("login !!!");
 });
 
+router.get('/twitter', (req, res) => {
+  consumer.getOAuthRequestToken(function (error, oauthToken, oauthTokenSecret, results) {
+    console.log("yobaretayo");
+    console.log(error);
+    console.log(oauthToken);
+    if (error) {
+      res.send(error, 500);
+      console.log(error);
+    } else {
+      req.session.oauthRequestToken = oauthToken;
+      req.session.oauthRequestTokenSecret = oauthTokenSecret;
+      const redirect = {
+        redirectUrl: `https://twitter.com/oauth/authorize?oauth_token=${req.session.oauthRequestToken}`
+      }
+      res.send(redirect);
+    }
+  });
+});
+
+router.get('twitter/saveAccessTokens', (req, res) => {
+  consumer.getOAuthAccessToken(
+    req.query.oauth_token,
+    req.session.oauthRequestTokenSecret,
+    req.query.oauth_verifier,
+    (error, oauthAccessToken, oauthAccessTokenSecret, results) => {
+      if (error) {
+        logger.error(error);
+        res.send(error, 500);
+      }
+      else {
+        req.session.oauthAccessToken = oauthAccessToken;
+        req.session.oauthAccessTokenSecret = oauthAccessTokenSecret
+        return res.send({ message: 'token saved' });
+      }
+    });
+});
 module.exports = router;
