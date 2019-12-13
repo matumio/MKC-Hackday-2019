9296e9fec98e1fe4dc41a1fe35be4340def2fb08
Meri <twinkling.star.m.t@gmail.com>
2019-12-13 23:06:42 +0900

    とりあえず commit

--- Hackday-2019/routes/auth.js
+++ Hackday-2019/routes/auth.js
@@ -1,13 +1,17 @@
+/* 
+認証を行う処理を配置している。
+*/
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
+// Twitter の認証APIにリクエストする
 router.get('/twitter', passport.authenticate('twitter'), function (req, res, next) {
   console.log(req, res, next);
   res.send('get twitter');
 });
 
-// /oauth/callbackにアクセスした時（Twitterログイン後）
+// /auth/twitter/callbackにアクセスした時（Twitterログイン後）
 router.get('/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), function (req, res) {
   console.log("im login");
   res.redirect('/'); //indexへリダイレクトさせる
