01a9986e2f66039a3914ce866cdf8e6807015364
松下海央 <matu@mio.yokohama>
2019-12-14 20:49:17 +0900

    カメラ画像を表示するPUGを新規追加

--- Hackday-2019/routes/index.js
+++ Hackday-2019/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express', session: req.session.passport });
+  res.render('face', { title: 'Express', session: req.session.passport });
 });
 
 module.exports = router;
